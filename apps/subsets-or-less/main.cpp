c45122de05620026630f10f5dffa4161d6597867
Masataro Asai <guicho2.71828@gmail.com>
2024-11-25 15:58:30 -0500

    subset-or-less

---
+++ apps/subsets-or-less/main.cpp
@@ -0,0 +1,84 @@
+
+#include <string>
+#include <sstream>
+#include <fstream>
+
+#include <tdzdd/DdStructure.hpp>
+
+using namespace tdzdd;
+using namespace std;
+
+using State = int;
+using Level = int;
+
+class Subsets: public DdSpec<Subsets,State,2> {
+    int const n;
+    int const k;
+
+public:
+    Subsets(int n, int k)
+            : n(n), k(k) {
+    }
+
+    Level getRoot(State& state) const {
+        state = 0;
+        return n;
+    }
+
+    Level getChild(State& state, Level level, int value) const {
+        state += value;
+        if (--level == 0) return (state == k) ? -1 : 0;
+        if (state > k) return 0;
+        if (state + level < k) return 0;
+        return level;
+    }
+};
+
+void usage(const char* programName) {
+    cerr << "Usage: " << programName << " n k " << endl;
+    cerr << "This program generates dot files for a ZDD containing all subsets of a set contianing 1..n whose size is less than k." << endl;
+}
+
+int main(int argc, char *argv[]) {
+
+    int n;
+    int k;
+
+    // Check if the correct number of arguments is provided
+    if (argc != 3) {
+        usage(argv[0]);
+        return 1;
+    }
+
+    // Parse the integers
+    try {
+        n = stoi(argv[1]);
+        k = stoi(argv[2]);
+
+        // Output the parsed integers
+        cout << "First integer: " << n << endl;
+        cout << "Second integer: " << k << endl;
+    } catch (const invalid_argument& e) {
+        cerr << "Error: Invalid integer input." << endl;
+        usage(argv[0]);
+        return 1;
+    } catch (const out_of_range& e) {
+        cerr << "Error: Integer out of range." << endl;
+        usage(argv[0]);
+        return 1;
+    }
+
+    Subsets spec(n, k);
+    ofstream os1(to_string(n) + "-" + to_string(k) + "-spec.dot");
+    spec.dumpDot(os1);
+
+    DdStructure<2> dd(spec);
+    ofstream os2(to_string(n) + "-" + to_string(k) + "-dd-before-reduced.dot");
+    dd.dumpDot(os2);
+
+    dd.zddReduce();
+    ofstream os3(to_string(n) + "-" + to_string(k) + "-dd-after-reduced.dot");
+    dd.dumpDot(os3);
+
+    return 0;
+}
